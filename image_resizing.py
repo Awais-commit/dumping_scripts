b4aa30e42a50881b39d2454562b5d4c39fd30018
Awais Akhtar <53480734+Awais-commit@users.noreply.github.com>
2022-04-25 12:08:04 +0500

    Update image_resizing.py

--- image_resizing.py
+++ image_resizing.py
@@ -1,13 +1,14 @@
 import numpy as np
-import os
-from os import listdir
-import glob
 import cv2
+import glob
+
+
+path_read = '/media/risc/8gb_usb/rp2/a'
+path_write = '/media/risc/8gb_usb/rp2/resized/'
 count = 0
-path = '/media/risc/8gb_usb/rp2/a'
-for image in glob.glob(path + "/*.png"):
+for image in glob.glob(path_read + "/*.png"):
 	count = count+1
 	frame = cv2.imread(image)
 	#print(type(frame))
 	resized = cv2.resize(frame, (32, 32), interpolation = cv2.INTER_NEAREST)
-	cv2.imwrite("%i.png"%count,resized)
+	cv2.imwrite(path_write + "%i.png"%count,resized)
